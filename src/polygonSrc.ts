715b956e31bdcc0719ed811a799bba27ed5b5dca
Ruby Quail <ruby@rubyquail.design>
2020-04-04 21:27:59 +1100

    starting to trasform the d3 code
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

---
+++ src/polygonSrc.ts
@@ -0,0 +1,115 @@
+import { Voronoi } from 'd3-delaunay';
+type extent = [number, number, number, number];
+function regioncode(x: number, y: number, [xmin, ymin, xmax, ymax]: extent) {
+    return (
+        (x < xmin ? 0b0001 : x > xmax ? 0b0010 : 0b0000) |
+        (y < ymin ? 0b0100 : y > ymax ? 0b1000 : 0b0000)
+    );
+}
+function _clipSegment(
+    x0: number,
+    y0: number,
+    x1: number,
+    y1: number,
+    c0: number,
+    c1: number,
+    ex: extent
+) {
+    const [xmin, ymin, xmax, ymax] = ex;
+
+    while (true) {
+        if (c0 === 0 && c1 === 0) return [x0, y0, x1, y1];
+        if (c0 & c1) return null;
+        let x, y;
+        const c = c0 || c1;
+
+        if (c & 0b1000)
+            (x = x0 + ((x1 - x0) * (ymax - y0)) / (y1 - y0)), (y = ymax);
+        else if (c & 0b0100)
+            (x = x0 + ((x1 - x0) * (ymin - y0)) / (y1 - y0)), (y = ymin);
+        else if (c & 0b0010)
+            (y = y0 + ((y1 - y0) * (xmax - x0)) / (x1 - x0)), (x = xmax);
+        else (y = y0 + ((y1 - y0) * (xmin - x0)) / (x1 - x0)), (x = xmin);
+        if (c0) (x0 = x), (y0 = y), (c0 = regioncode(x0, y0, ex));
+        else (x1 = x), (y1 = y), (c1 = regioncode(x1, y1, ex));
+    }
+}
+
+function _renderSegment(x0: number, y0: number, x1: number, y1: number) {
+    let context: any;
+
+    const c0 = this._regioncode(x0, y0);
+    const c1 = this._regioncode(x1, y1);
+
+    if (c0 === 0 && c1 === 0) {
+        return [x0, y0, x1, y1];
+    }
+    const S = this._clipSegment(x0, y0, x1, y1, c0, c1);
+
+    if (S) return S as [number, number, number, number];
+}
+function _project(x0: number, y0: number, vx: number, vy: number, ex: extent) {
+    const [xmin, ymin, xmax, ymax] = ex;
+    let t = Infinity,
+        c: number,
+        x: number,
+        y: number;
+
+    if (vy < 0) {
+        // top
+        if (y0 <= ymin) return null;
+        if ((c = (ymin - y0) / vy) < t) (y = ymin), (x = x0 + (t = c) * vx);
+    } else if (vy > 0) {
+        // bottom
+        if (y0 >= ymax) return null;
+        if ((c = (ymax - y0) / vy) < t) (y = ymax), (x = x0 + (t = c) * vx);
+    }
+    if (vx > 0) {
+        // right
+        if (x0 >= xmax) return null;
+        if ((c = (xmax - x0) / vx) < t) (x = xmax), (y = y0 + (t = c) * vy);
+    } else if (vx < 0) {
+        // left
+        if (x0 <= xmin) return null;
+        if ((c = (xmin - x0) / vx) < t) (x = xmin), (y = y0 + (t = c) * vy);
+    }
+
+    return [x, y];
+}
+
+function render(vor: Voronoi<any>) {
+    const {
+        delaunay: { halfedges, inedges, hull: hll },
+        circumcenters,
+        vectors
+    } = vor;
+    const hull = hll as any;
+
+    if ((hull as any).length <= 1) return null;
+    for (let i = 0, n = halfedges.length; i < n; ++i) {
+        const j = halfedges[i];
+
+        if (j < i) continue;
+        const ti = Math.floor(i / 3) * 2;
+        const tj = Math.floor(j / 3) * 2;
+        const xi = circumcenters[ti];
+        const yi = circumcenters[ti + 1];
+        const xj = circumcenters[tj];
+        const yj = circumcenters[tj + 1];
+
+        this._renderSegment(xi, yi, xj, yj);
+    }
+    let h0,
+        h1 = hull[hull.length - 1];
+
+    for (let i = 0; i < hull.length; ++i) {
+        (h0 = h1), (h1 = hull[i]);
+        const t = Math.floor(inedges[h1] / 3) * 2;
+        const x = circumcenters[t];
+        const y = circumcenters[t + 1];
+        const v = h0 * 4;
+        const p = this._project(x, y, vectors[v + 2], vectors[v + 3]);
+
+        if (p) this._renderSegment(x, y, p[0], p[1]);
+    }
+}
