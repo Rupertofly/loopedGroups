e05bcc9336eb51f120a79a2997cc955ac01ea6a5
Ruby Quail <ruby@rubyquail.design>
2020-04-05 21:33:32 +1000

    happy ruby is happy
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- test/test.ts
+++ test/test.ts
@@ -2,13 +2,14 @@ document.body.innerText = 'test Successful';
 import * as d from 'd3-delaunay';
 import * as ge from '../src/getEdges';
 import { render } from '../src/polygonSrc';
+type pt = [number, number];
 const pts: [number, number][] = [];
 
-for (let i = 0; i < 64; i++) {
-    pts.push([5 + Math.random() * 795, 5 + Math.random() * 795]);
+for (let i = 0; i < 256; i++) {
+    pts.push([5 + Math.random() * 300, 5 + Math.random() * 500]);
 }
-const diag = d.Delaunay.from(pts);
-const nikPantis = diag.voronoi([5, 5, 800, 800]);
+let diag = d.Delaunay.from(pts);
+let nikPantis = diag.voronoi([5, 5, 800, 800]);
 const cv = document.createElement('canvas');
 
 cv.width = 805;
@@ -17,7 +18,26 @@ const ctx = cv.getContext('2d');
 
 ctx.fillStyle = '#eeeeee';
 console.log(nikPantis);
+function centroid(polygon: pt[]) {
+    const n = polygon.length;
+    let i = -1,
+        x = 0,
+        y = 0,
+        a: pt,
+        b = polygon[n - 1],
+        c,
+        k = 0;
 
+    while (++i < n) {
+        a = b;
+        b = polygon[i];
+        k += c = a[0] * b[1] - b[0] * a[1];
+        x += (a[0] + b[0]) * c;
+        y += (a[1] + b[1]) * c;
+    }
+
+    return (k *= 3), [x / k, y / k] as pt;
+}
 document.body.append(cv);
 // for (let i = 0; i < diag.points.length; i += 2) {
 //     ctx.fillStyle = '#ff000088';
@@ -25,13 +45,23 @@ document.body.append(cv);
 //     ctx.fillRect(diag.points[i] - 4, diag.points[i + 1] - 4, 8, 8);
 // }
 function renderFrame() {
+    ctx.clearRect(0, 0, 805, 805);
+    diag = d.Delaunay.from(pts);
+    nikPantis = diag.voronoi([5, 5, 800, 800]);
+    for (let i = 0; i < pts.length; i++) {
+        const pg = nikPantis.cellPolygon(i);
+        const cx = centroid(pg as pt[]);
+        const newPT = pts[i].map((v, j) => v + (cx[j] - v) * 0.1) as pt;
+
+        pts[i] = newPT;
+    }
     for (const ed of render(nikPantis)) {
         ctx.strokeStyle = '#000000ff';
         if (!ed.line) continue;
-        ctx.beginPath();
-        ctx.moveTo(...ed.line[0]);
-        ctx.lineTo(...ed.line[1]);
-        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.moveTo(...ed.line[0]);
+        // ctx.lineTo(...ed.line[1]);
+        // ctx.stroke();
         if (ed.left != null && ed.right != null) {
             const l = [
                 diag.points[ed.left * 2],
@@ -61,5 +91,6 @@ function renderFrame() {
             ctx.stroke();
         }
     }
+    window.requestAnimationFrame(renderFrame);
 }
 renderFrame();
